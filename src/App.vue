c1f518c6a2e4e2402f02699d5d8c784255fdb615
Damian <przpiw@gmail.com>
2021-05-21 21:27:45 +0930

    Axios request added

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
-  <section class="score">
-    Player <span>1</span> x <span>2</span> Computer
-  </section>
-  <h1>The communication protocol NFC stands for Near-Field Control.</h1>
-  <input type="radio" name="options" value="True" />
-  <label>True</label>
-  <br />
-  <input type="radio" name="options" value="False" />
-  <label>False</label>
-  <br />
-  <button class="send" type="button">Send</button>
+  <div>
+    <section class="score">
+      Player <span>1</span> x <span>2</span> Computer
+    </section>
+    <h1>The communication protocol NFC stands for Near-Field Control.</h1>
+    <input type="radio" name="options" value="True" />
+    <label>True</label>
+    <br />
+    <input type="radio" name="options" value="False" />
+    <label>False</label>
+    <br />
+    <button class="send" type="button">Send</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "App",
+  name: 'App',
+  created() {
+    this.axios
+      .get('https://opentdb.com/api.php?amount=1&category=18')
+      .then((response) => {
+        console.log(response.data.results[0])
+      })
+  },
   components: {},
-};
+}
 </script>
 
 <style lang="scss">
@@ -29,7 +38,7 @@ export default {
   margin: 60px auto;
   max-width: 960px;
 
-  input[type="radio"] {
+  input[type='radio'] {
     margin: 12px 4px;
   }
 
