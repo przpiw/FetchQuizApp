96a46b373d6ce760f8d7a8eb6e60850e777f4c03
Damian <przpiw@gmail.com>
2021-05-22 03:07:54 +0930

    Fetch Quiz app vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,119 @@
 <template>
   <div>
-    <section class="score">
-      Player <span>1</span> x <span>2</span> Computer
-    </section>
-    <h1>The communication protocol NFC stands for Near-Field Control.</h1>
-    <input type="radio" name="options" value="True" />
-    <label>True</label>
-    <br />
-    <input type="radio" name="options" value="False" />
-    <label>False</label>
-    <br />
-    <button class="send" type="button">Send</button>
+    <ScoreBoard
+      :correctCount="this.correctCount"
+      :incorrectCount="this.incorrectCount"
+    />
+    <template v-if="this.question">
+      <h1 v-html="this.question"></h1>
+      <div v-for="(answer, index) in this.answers" :key="index">
+        <input
+          :disabled="this.answerSubmitted"
+          type="radio"
+          name="options"
+          :value="answer"
+          v-model="this.chosenAnswer"
+        />
+        <label v-html="answer"></label>
+      </div>
+      <br />
+      <button
+        v-if="!this.answerSubmitted"
+        @click="this.submitAnswer()"
+        class="send"
+        type="button"
+      >
+        Submit
+      </button>
+
+      <button
+        v-if="this.answerSubmitted"
+        @click="this.getNewQuestion()"
+        class="send"
+        type="button"
+      >
+        Next
+      </button>
+
+      <section v-if="answerSubmitted" class="result">
+        <h4
+          class="incorrect"
+          v-if="
+            this.chosenAnswer != this.correctAnswer &&
+              this.chosenAnswer != 'undefined'
+          "
+          v-html="'Incorrect, the correct answer is ' + this.correctAnswer"
+        ></h4>
+        <h4 class="correct" v-else v-html="' Correct.'"></h4>
+      </section>
+    </template>
   </div>
 </template>
 
 <script>
+import ScoreBoard from '@/components/ScoreBoard.vue'
+
 export default {
   name: 'App',
-  created() {
-    this.axios
-      .get('https://opentdb.com/api.php?amount=1&category=18')
-      .then((response) => {
-        console.log(response.data.results[0])
-      })
+  components: {
+    ScoreBoard,
   },
-  components: {},
+  data() {
+    return {
+      question: undefined,
+      incorrectAnswers: undefined,
+      correctAnswer: undefined,
+      chosenAnswer: undefined,
+      answerSubmitted: false,
+      correctCount: 0,
+      incorrectCount: 0,
+    }
+  },
+  computed: {
+    answers() {
+      var answers = JSON.parse(JSON.stringify(this.incorrectAnswers))
+      answers.splice(
+        Math.round(Math.random() * answers.length),
+        0,
+        this.correctAnswer
+      )
+      return answers
+    },
+  },
+  methods: {
+    submitAnswer() {
+      if (!this.chosenAnswer) alert('Pick one of the options')
+      else {
+        this.answerSubmitted = true
+        if (this.chosenAnswer == this.correctAnswer) {
+          this.correctCount++
+        } else this.incorrectCount++
+      }
+    },
+    getNewQuestion() {
+      this.answerSubmitted = false
+      this.chosenAnswer = undefined
+      this.question = undefined
+      this.axios({
+        method: 'GET',
+        url: 'https://opentdb.com/api.php?amount=1&category=18',
+        timeout: 1000,
+      })
+        .then((response) => {
+          this.question = response.data.results[0].question
+          this.incorrectAnswers = response.data.results[0].incorrect_answers
+          this.correctAnswer = response.data.results[0].correct_answer
+        })
+        .catch((error) => {
+          console.error(error)
+          //if timeout request again
+          this.getNewQuestion()
+        })
+    },
+  },
+  created() {
+    if (!this.answerSubmitted) this.getNewQuestion()
+  },
 }
 </script>
 
@@ -51,6 +140,12 @@ export default {
     background-color: #1867c0;
     border: 1px solid #1867c0;
     cursor: pointer;
+  }
+  .correct {
+    color: green;
+  }
+  .incorrect {
+    color: red;
   }
 }
 </style>
